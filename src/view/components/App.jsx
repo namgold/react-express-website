4f700dd1cef70e89e5a77da1014a3398da6d1f85
Nam Nguyen <hnamnguyencse@gmail.com>
2020-10-05 23:36:57 +0700

    abc

--- src/view/components/App.jsx
+++ src/view/components/App.jsx
@@ -4,12 +4,20 @@ import '../css/App.css';
 import T from '../js/common';
 
 class App extends React.Component {
+    state = { connecting: false }
 
-    testConnect(e) {
+    testConnect = (e) => {
         e.preventDefault();
+        this.setState({ connecting: true });
         T.get("/",
-            data => T.notify("API connect success", T.NOTIFY_TYPE.SUCCESS),
-            error => T.notify("API connect fail", T.NOTIFY_TYPE.DANGER));
+            data => {
+                T.notify("API connect success", T.NOTIFY_TYPE.SUCCESS);
+                this.setState({ connecting: false });
+            },
+            error => {
+                T.notify("API connect fail", T.NOTIFY_TYPE.DANGER)
+                this.setState({ connecting: false })
+            });
     }
 
     render() {
@@ -20,7 +28,10 @@ class App extends React.Component {
                     <p>Edit <code>src/view/components/App.js</code> and save to reload.</p>
                     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
                     <br/>
-                    <button className='btn btn-success' onClick={this.testConnect}>Test API connection</button>
+                    <button className='btn btn-success' onClick={this.testConnect}>
+                        Test API connection
+                    </button>
+                    <span style={{visibility: this.state.connecting ? 'visible' : 'hidden'}}><i className="fas fa-sync fa-spin"></i></span>
                 </header>
             </div>
         );
